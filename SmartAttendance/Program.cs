f58a642408061274975eb116900a580f4db014ec
lenardoormeno-ausi <leonardo.ormeno@mi.unc.edu.ar>
2026-06-23 16:40:40 -0300

    Configura autenticacion JWT

--- SmartAttendance/Program.cs
+++ SmartAttendance/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using SmartAttendance.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,25 @@ builder.Services.AddOpenApi();
 
 // Dependency Injection
 builder.Services.AddSingleton<IStudentService, InMemoryStudentService>();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            "CLAVE_SUPER_SECRETA_TP10"))
+            };
+    });
+
+builder.Services.AddAuthorization();
 
 // CORS (para React futuro)
 builder.Services.AddCors(options =>
@@ -34,6 +56,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("Frontend");
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Map Controllers
 app.MapControllers();
